abecfb78c32cf2662a7f99ee31489a890ac256fe
mqiu <mqiu@gocardless.com>
2025-09-08 15:54:32 +0100

    draft

--- lib/nandi/safe_migration_enforcer.rb
+++ lib/nandi/safe_migration_enforcer.rb
@@ -7,7 +7,6 @@ require "rails/generators"
 require "nandi/file_diff"
 require "nandi/file_matcher"
 require "nandi/lockfile"
-require "nandi/violations"
 
 module Nandi
   class SafeMigrationEnforcer
@@ -22,11 +21,6 @@ module Nandi
                    ar_migration_dir: DEFAULT_AR_MIGRATION_DIR,
                    files: DEFAULT_FILE_SPEC)
       @files = files
-
-      @ungenerated_violations = Violations::UngeneratedMigrationViolations.new
-      @hand_written_violations = Violations::HandWrittenMigrationViolations.new
-      @out_of_date_violations = Violations::OutOfDateMigrationViolations.new
-      @hand_edited_violations = Violations::HandEditedMigrationViolations.new
 
       require require_path unless require_path.nil?
 
@@ -45,19 +39,10 @@ module Nandi
     end
 
     def run
-      Nandi.config.databases.each do |database|
-        check_database_migrations(database)
-      end
+      errors = collect_errors_from_databases
 
-      all_violations = [
-        @ungenerated_violations,
-        @hand_written_violations,
-        @out_of_date_violations,
-        @hand_edited_violations,
-      ].select(&:any?)
-
-      if all_violations.any?
-        raise MigrationLintingFailed, all_violations.map(&:to_s).join("\n\n")
+      if errors.any?
+        raise MigrationLintingFailed, errors.join("\n\n")
       end
 
       true
@@ -65,16 +50,22 @@ module Nandi
 
     private
 
-    def check_database_migrations(database_config)
-      safe_migrations = matching_migrations(database_config.migration_directory)
-      ar_migrations = matching_migrations(database_config.output_directory)
+    def collect_errors_from_databases
+      errors = []
 
-      return if safe_migrations.none? && ar_migrations.none?
+      Nandi.config.databases.each do |database|
+        safe_migrations = matching_migrations(database.migration_directory)
+        ar_migrations = matching_migrations(database.output_directory)
 
-      @ungenerated_violations.add_violations(safe_migrations:, ar_migrations:)
-      @hand_written_violations.add_violations(safe_migrations:, ar_migrations:)
-      @hand_edited_violations.add_violations(ar_migrations:, database_config:)
-      @out_of_date_violations.add_violations(safe_migrations:, database_config:)
+        next if safe_migrations.none? && ar_migrations.none?
+
+        errors.concat(check_ungenerated_migrations(safe_migrations, ar_migrations))
+        errors.concat(check_hand_written_migrations(safe_migrations, ar_migrations))
+        errors.concat(check_out_of_date_migrations(safe_migrations, database))
+        errors.concat(check_hand_edited_migrations(ar_migrations, database))
+      end
+
+      errors
     end
 
     def matching_migrations(dir)
@@ -82,6 +73,79 @@ module Nandi
 
       names = Dir.glob(File.join(dir, "*.rb")).map { |path| File.basename(path) }
       FileMatcher.call(files: names, spec: @files)
+    end
+
+    def check_ungenerated_migrations(safe_migrations, ar_migrations)
+      ungenerated_migrations = safe_migrations - ar_migrations
+      return [] if ungenerated_migrations.empty?
+
+      [<<~ERROR.strip]
+        The following migrations are pending generation:
+
+          - #{ungenerated_migrations.sort.join("\n  - ")}
+
+        Please run `rails generate nandi:compile` to generate your migrations.
+      ERROR
+    end
+
+    def check_hand_written_migrations(safe_migrations, ar_migrations)
+      handwritten_migrations = ar_migrations - safe_migrations
+      return [] if handwritten_migrations.empty?
+
+      [<<~ERROR.strip]
+        The following migrations have been written by hand, not generated:
+
+          - #{handwritten_migrations.sort.join("\n  - ")}
+
+        Please use Nandi to generate your migrations. In exeptional cases, hand-written
+        ActiveRecord migrations can be added to the .nandiignore file. Doing so will
+        require additional review that will slow your PR down.
+      ERROR
+    end
+
+    def check_out_of_date_migrations(safe_migrations, database_config)
+      out_of_date_migrations = safe_migrations.
+        map { |m| [m, Nandi::Lockfile.for(database_config.name).get(file_name: m)] }.
+        select do |filename, digests|
+          Nandi::FileDiff.new(
+            file_path: File.join(database_config.migration_directory, filename),
+            known_digest: digests[:source_digest],
+          ).changed?
+        end
+
+      return [] if out_of_date_migrations.empty?
+
+      [<<~ERROR.strip]
+        The following migrations have changed but not been recompiled:
+
+          - #{out_of_date_migrations.map(&:first).sort.join("\n  - ")}
+
+        Please recompile your migrations to make sure that the changes you expect are
+        applied.
+      ERROR
+    end
+
+    def check_hand_edited_migrations(ar_migrations, database_config)
+      hand_altered_migrations = ar_migrations.
+        map { |m| [m, Nandi::Lockfile.for(database_config.name).get(file_name: m)] }.
+        select do |filename, digests|
+          Nandi::FileDiff.new(
+            file_path: File.join(database_config.output_directory, filename),
+            known_digest: digests[:compiled_digest],
+          ).changed?
+        end
+
+      return [] if hand_altered_migrations.empty?
+
+      [<<~ERROR.strip]
+        The following migrations have had their generated content altered:
+
+          - #{hand_altered_migrations.map(&:first).sort.join("\n  - ")}
+
+        Please don't hand-edit generated migrations. If you want to write a regular
+        ActiveRecord::Migration, please do so and add it to .nandiignore. Note that
+        this will require additional review that will slow your PR down.
+      ERROR
     end
   end
 end
